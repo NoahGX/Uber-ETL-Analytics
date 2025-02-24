9623aa52701a5b83887fd20c15d42b9fd8da0a13
NoahGX <114901822+NoahGX@users.noreply.github.com>
2025-02-23 23:53:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 df.info()
 datetime_dim = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']].drop_duplicates().reset_index(drop=True)
 datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
+datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.hour
