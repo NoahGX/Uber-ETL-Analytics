f15f40836319ac9f9cd9005579aaa0be7c184d65
NoahGX <114901822+NoahGX@users.noreply.github.com>
2025-02-27 23:59:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 import pandas as pd
 df = read_csv("data/uber_data.csv")
 
-df.head()
-df.info()
-
 df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
 df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 
